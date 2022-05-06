98298a7aadc06866c60b10189d3241297f5c710b
VedranKasapovic <vedran.kasapovic@gmail.com>
2022-05-06 22:38:12 +0200

    Create frizerskisalon.sql
    preimenovan zbog greške Error dropping database (can't rmdir '.\frizerski_salon', errno: 41 "Directory not empty")

---
+++ frizerskisalon.sql
@@ -0,0 +1,39 @@
+# hash je komentar
+# Sljedeću liniju bez početnog hash znaka izvesti u cmd
+# C:\xampp\mysql\bin\mysql -uroot < C:\Users\Veky\Documents\GitHub\java26\frizerskisalon.sql
+
+drop database if exists frizerskisalon;
+create database frizerskisalon;
+use frizerskisalon;
+
+create table salon(
+    naziv varchar(50),
+    adresa varchar(50),
+    djelatnica varchar(50)
+);
+
+create table osoba(
+    ime varchar(50),
+    prezime varchar(50),
+    brojtelefona varchar(50)
+);
+
+create table djelatnica(
+    osoba varchar(50),
+    oib varchar(50),
+    smjena varchar(50),
+    iban varchar(50)
+);
+
+create table korisnik(
+    osoba varchar(50),
+    datumivrijeme varchar(50),
+    usluga varchar(50),
+    djelatnica varchar(50)
+);
+
+create table usluga(
+    cijena varchar(50),
+    naziv varchar(50),
+    trajanje varchar(50)
+);
